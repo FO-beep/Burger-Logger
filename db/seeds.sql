f196ffc32943b65a45da4f37fd0d4dd0a3540a6a
Faithful Okere <feitful20@gmail.com>
2020-05-30 15:18:49 -0400

    updated the seeds folder, got the server to start li stening

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name, devoured)
+VALUES ("Le Pigeon Burger",  FALSE), ("Whiskey King", FALSE),
+("Cheeseburger", FALSE),
+("Kimchi", FALSE),
+("Salmon", FALSE );
+("Veggie", FALSE );
