6142a561108433bf5fe827c102619147c9529ee6
Faithful Okere <feitful20@gmail.com>
2020-07-14 18:40:33 -0400

    updated path

--- server.js
+++ server.js
@@ -8,28 +8,31 @@ const app = express();
 app.use(express.static("public"));
 
 //Parse application body as JSON
-app.use(express.urlencoded({
-    extended: true
-}));
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
 app.use(express.json());
 
 //Set Handlebars.
 const exphbs = require("express-handlebars");
 
-app.engine("handlebars", exphbs({
-    defaultLayout: "main"
-}));
+app.engine(
+  "handlebars",
+  exphbs({
+    defaultLayout: "main",
+  })
+);
 app.set("view engine", "handlebars");
 
 //Import routes and give the server access to them.
-const routes = require("./controllers/burgerController.js");
+const routes = require("./controllers/burgerController");
 
 app.use(routes);
 
-
 //Start our server so that it can begin listening to clients requests.
 app.listen(PORT, function () {
-    //Log (server-side) when our server has started
-    console.log("Server is listening on: http://localhost:" + PORT);
-
+  //Log (server-side) when our server has started
+  console.log("Server is listening on: http://localhost:" + PORT);
 });
