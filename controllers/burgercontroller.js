62544668096545fd98f6e9c3f0aee4f13af3ec7f
Faithful Okere <feitful20@gmail.com>
2020-07-14 19:05:49 -0400

    updated file

--- controllers/burgercontroller.js
+++ controllers/burgercontroller.js
@@ -1,51 +1,55 @@
 const express = require("express");
-const router = express.Router();
+const router = express();
 const burger = require("../models/burger.js");
 
 // Routes
 router.get("/", function (req, res) {
-    // Get all burger data from db
-    burger.selectAll(function (data) {
-        res.render("index", {
-            burgers: data
-        });
+  // Get all burger data from db
+  burger.selectAll(function (data) {
+    res.render("index", {
+      burgers: data,
     });
+  });
 });
 
 router.post("/api/burgers", function (req, res) {
-    // Add new burger to db
+  // Add new burger to db
 
-    burger.insertOne(["burger_name"], [req.body.burger_name], function (data) {
-        res.redirect("/");
-    })
+  burger.insertOne(["burger_name"], [req.body.burger_name], function (data) {
+    res.redirect("/");
+  });
 });
 
 router.put("/api/burgers/:id", function (req, res) {
-    let condition = `id = ${req.params.id}`;
+  let condition = `id = ${req.params.id}`;
 
-    // Update burger entry in db
-    burger.updateOne({
-        devoured: req.body.devoured
-    }, condition, function (result) {
-        if (result.changedRows == 0) {
-            return res.status(404).end();
-        } else {
-            res.status(200).end();
-        }
-    });
+  // Update burger entry in db
+  burger.updateOne(
+    {
+      devoured: req.body.devoured,
+    },
+    condition,
+    function (result) {
+      if (result.changedRows == 0) {
+        return res.status(404).end();
+      } else {
+        res.status(200).end();
+      }
+    }
+  );
 });
 
 router.delete("/api/burgers/:id", function (req, res) {
-    let condition = `id = ${req.params.id}`;
+  let condition = `id = ${req.params.id}`;
 
-    // Delete burger entry in db
-    burger.delete(condition, function (result) {
-        if (result.affectedRows == 0) {
-            return res.status(404).end();
-        } else {
-            res.status(200).end();
-        }
-    })
-})
+  // Delete burger entry in db
+  burger.delete(condition, function (result) {
+    if (result.affectedRows == 0) {
+      return res.status(404).end();
+    } else {
+      res.status(200).end();
+    }
+  });
+});
 
 module.exports = router;
