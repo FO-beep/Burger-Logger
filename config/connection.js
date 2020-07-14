cba7d7a559e5038df8b4d13e36e47c7d2c5ccef9
Faithful Okere <feitful20@gmail.com>
2020-07-14 18:51:10 -0400

    updated connection

--- config/connection.js
+++ config/connection.js
@@ -1,33 +1,29 @@
-require('dotenv').config();
+require("dotenv").config();
 const mysql = require("mysql");
 
 let connection;
 
 // for jawsDB
 if (process.env.JAWSDB_URL) {
-    connection = mysql.createConnection(process.env.JAWSDB_URL);
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
-    connection = mysql.createConnection({
-        host: "localhost",
-        port: 3306,
-        user: DB_USERNAME,
-        password: DB_PASSWORD,
-        database: DATABASE
-
-    });
-};
-
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DATABASE,
+  });
+}
 
 //Make connection.
 connection.connect(function (err) {
-    if (err) {
-        console.error("error connecting: " + err.stack);
-        return;
-    }
-    console.log("connected as id " + connection.threadId);
-
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
 });
-
 
 //Export connection for our ORM to use.
 module.exports = connection;
